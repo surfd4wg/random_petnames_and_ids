6c238489384aa316b43e995de35c3c90f29b9111
surfd4wg <surfd4wg@gmail.com>
2022-05-26 18:09:48 -0600

    all

---
+++ locals.tf
@@ -0,0 +1,17 @@
+locals {
+    common_tags = {
+        Name = "${var.myname}-${random_id.server.hex}" 
+	    Environment = var.myenv
+        Department  = var.mydept
+        Organization = var.myorg
+        Stack       = var.mystack
+        "***RANDOMid***" = "${var.myname}-${random_id.server.id}" 
+        "***RANDOMhex***" = "${var.myname}-${random_id.server.hex}" 
+        "***RANDOMdec***" = "${var.myname}-${random_id.server.dec}" 
+        "***PETid***" = "${var.myname}-${random_pet.server.id}" 
+        RandomPETName = "${random_pet.server.id}-${random_id.server.id}"
+        #"***PEThex***" = "${var.myname}-${random_pet.server.hex}" 
+        #"***PETdec***" = "${var.myname}-${random_pet.server.dec}" 
+        "***WORKSPACE***" = terraform.workspace
+		}
+	}
